6317bd3a746a594358b2335788b2eebbb92fd7b8
Jon Goldson <forjrg@gmail.com>
2021-07-10 08:58:14 -0600

    Adding in additional front end components, created multiple routes (pages) between Home, Blocks, and a form to conduct transactions

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Blocks from './Blocks';
+import { Link } from 'react-router-dom';
 import logo from '../assets/LogoLarge.png';
 
 class App extends Component {
@@ -20,12 +20,16 @@ class App extends Component {
                     <br />
                 </div>
                 <div>Welcome to SHUECOIN</div>
+                <br />
+                <div><Link to='/blocks'>Blocks</Link></div>
+                <div><Link to='/conduct-transaction'>Conduct a Transaction</Link></div>
+                <br />
                 <div className='WalletInfo'>
                 <div>Address: {address}</div>
                 <div>Balance: {balance}</div>
                 </div>
-                <br />
-                <Blocks />
+                
+                
             </div>
         );
     }
